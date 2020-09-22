6e4a7db21a09fc225d2b628b16fbacaab9ca0061
Kyle Bloyd <bloyd_kyle_n@cat.com>
2020-09-21 22:06:00 -0500

    setting up our react app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "kblizoid-notes",
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://adftul2by4.execute-api.us-east-2.amazonaws.com/prod",
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_2QUYHBp4l",
+    APP_CLIENT_ID: "6jvg52kelglc652qsumr7t91gm",
+    IDENTITY_POOL_ID: "us-east-2:35b8870d-5bec-47ad-99b0-6550b2cfd624",
+  },
+};
